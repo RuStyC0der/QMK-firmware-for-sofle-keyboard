ce0b662bf145c9ef20b7aecda8b3e88ee2407463
RuStyC0der <megausermind@gmail.com>
2025-09-20 13:22:17 +0300

    add macros

--- sofle/sofle/keymaps/vialj64/oled.c
+++ sofle/sofle/keymaps/vialj64/oled.c
@@ -1117,6 +1117,10 @@ bool oled_task_user(void) {
     return false;
 }
 
+enum custom_keycodes {
+    M0 = SAFE_RANGE,
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case KC_LCTL:
@@ -1134,6 +1138,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 isJumping = false;
             }
+            break;
+		case M0:
+			if (record->event.pressed) {
+				SEND_STRING(SS_DOWN(X_LGUI)SS_TAP(X_SPACE)SS_UP(X_LGUI));
+            } else {
+				// when keycode QMKBEST is released
+			}
             break;
     }
     return true; // Let QMK send the enter press/release events
