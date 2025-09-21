e76ab913054614c6ec988ebf7c43530ca377056c
RuStyC0der <megausermind@gmail.com>
2025-09-21 20:18:01 +0300

    add quick tap term config

--- sofle/sofle/keymaps/vialj64/tap_hold.c
+++ sofle/sofle/keymaps/vialj64/tap_hold.c
@@ -12,3 +12,14 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
 }
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1, KC_SPC):
+            return 50;
+        case LT(2, KC_ENT):
+            return 50; 
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
