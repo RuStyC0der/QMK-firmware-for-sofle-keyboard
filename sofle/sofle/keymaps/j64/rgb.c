7a3d4aead48098b272ed894bf2f3bcc2c5cc87ff
RuStyC0der <megausermind@gmail.com>
2026-05-30 16:17:54 +0300

    change whole keeb color when gaming layer is active

--- sofle/sofle/keymaps/j64/rgb.c
+++ sofle/sofle/keymaps/j64/rgb.c
@@ -1,11 +1,21 @@
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-    case 3:
-        rgb_matrix_set_color(0, 32, 0, 0);
-        break;
-    default: //  for any other layers, or the default layer
-        rgb_matrix_set_color(0, 0,0,0);
-        break;
+#define J64_GAME_LAYER 3
+
+static void j64_apply_layer_rgb(layer_state_t state) {
+    if (get_highest_layer(state) == J64_GAME_LAYER) {
+        for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+            if (g_led_config.flags[i] != LED_FLAG_NONE) {
+                rgb_matrix_set_color(i, 255, 0, 0);
+            }
+        }
     }
-  return state;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    j64_apply_layer_rgb(state);
+    return state;
+}
+
+bool rgb_matrix_indicators_user(void) {
+    j64_apply_layer_rgb(layer_state);
+    return false;
 }
--- sofle/sofle/keymaps/j64/suspend.c
+++ sofle/sofle/keymaps/j64/suspend.c
@@ -1,5 +1,3 @@
 void suspend_wakeup_init_user(void) {
-    if (layer_state_is(3)) {
-        rgb_matrix_set_color(0, 32, 0, 0);
-    }
+    j64_apply_layer_rgb(layer_state);
 }
